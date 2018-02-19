40b2db3496cb6f2a64444b0c377cda476626f853
Eli de Smet <tapir@wcw-stud-145-109-63-219.wireless.uva.nl>
2018-02-19 11:48:37 +0100

    Niet eerder gecommit

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,6 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // initializing variables
+    var brightness: Double = 1
+    var counter = 0
+    @IBOutlet weak var text: UILabel!
+    
+    // function to update brightness lighter
+    func lighter(){
+        if brightness < 1 {
+            brightness += 0.1
+        }
+    }
+    
+    // function to update brightness darker
+    func darker(){
+        if brightness > 0 {
+            brightness -= 0.1
+        }
+    }
+    
+    // function to update UI
+    func updateUI(){
+        view.backgroundColor = UIColor.init(white: CGFloat(brightness), alpha: 1)
+        
+        // protecting user from using the app too much
+        counter += 1
+        if counter > 15 {
+            text.text = "MAKE BETTER USE OF YOUR TIME!"
+            if brightness > 0.5 {
+                text.textColor = .black
+            } else {
+                text.textColor = .white
+            }
+        }
+    }
+    
+    // plus button pressed
+    @IBAction func plusPressed(_ sender: Any) {
+        lighter()
+        updateUI()
+    }
+    
+    // minus button pressed
+    @IBAction func minusPressed(_ sender: Any) {
+        darker()
+        updateUI()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +67,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // hidding status bar for better visuals
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
 }
 
